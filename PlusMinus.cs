f6193795e850f85e36e29feb7a8bb9541660bbbe
bananamyi8 <ronelynsantillan15@gmail.com>
2025-10-01 20:08:39 +0800

    Add files via upload

---
+++ PlusMinus.cs
@@ -0,0 +1,50 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'plusMinus' function below.
+     *
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static void plusMinus(List<int> arr)
+    {
+        int n = arr.Count;
+        int positive = arr.Count(x => x > 0);
+        int negative = arr.Count(x => x < 0);
+        int zero = arr.Count(x => x == 0);
+
+        Console.WriteLine(((double)positive / n).ToString("F6"));
+        Console.WriteLine(((double)negative / n).ToString("F6"));
+        Console.WriteLine(((double)zero / n).ToString("F6"));
+    }
+
+
+
+
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+            Result.plusMinus(arr);
+        }
+    }
+}
